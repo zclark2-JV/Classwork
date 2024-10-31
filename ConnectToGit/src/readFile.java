a9ba65bcf6934e4ff513a1ab5bfb977a216b18ac
zac <zac@localhost>
2024-10-31 10:07:34 -0400

    a

--- ConnectToGit/src/readFile.java
+++ ConnectToGit/src/readFile.java
@@ -3,13 +3,22 @@ import java.io.*; //IO Input and Output
 public class readFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
 		Scanner inFile = new Scanner(new FileReader("payroll.txt"));
 		String firstName;
 		String lastName;
+		double wages;
 		double hoursWorked;
 		double payRate;
 		
-		
+		firstName=inFile.next();
+		lastName=inFile.next();
+		hoursWorked=inFile.nextDouble();
+		payRate=inFile.nextDouble();
+		inFile.close();
+		wages=hoursWorked*payRate;
+		System.out.println(firstName+" "+lastName+" Wages: $"+wages);
 	}
 
 }
